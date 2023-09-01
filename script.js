418d204bc5391dea954e43183a6b174935d20138
jainil <67572984+jainilt@users.noreply.github.com>
2023-09-01 16:47:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var elm1 = document.querySelector("#elm1");
+var elm = document.querySelectorAll(".elm");
+
+elm.forEach(function(val){
+    val.addEventListener("mouseenter",function(){
+        val.childNodes[3].style.opacity = 1;
+    })
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[3].style.opacity = 0;
+    })
+    val.addEventListener("mousemove",function(dets){
+        val.childNodes[3].style.left = dets.x+"px";
+    })
+})
